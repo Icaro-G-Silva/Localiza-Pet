31985d10384a1aa8a461ab30d1964d65e7fc9750
Icaro-G-Silva <icaro.igs@pm.me>
2023-10-03 01:01:58 -0300

    Add frontend structure

---
+++ frontend/localiza-pet-frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Feed from './pages/feed';
+
+const App: React.FC = () => {
+  return (
+    <Feed />
+  );
+}
+
+export default App;
---
+++ frontend/localiza-pet-frontend/src/pages/feed/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FaAt } from "react-icons/fa6";
+import { Container } from "./styles";
+
+const Feed: React.FC = () => {
+    return (
+        <Container>
+            FEED!&nbsp;<FaAt />
+        </Container>
+    );
+}
+
+export default Feed;
